6bccc0387526997cefbbb8d404349bc34ed630af
Eric Lam <ericlamyauping@gmail.com>
2017-10-03 19:25:31 +1000

    Company name resource file

--- notatsukinotanoshi/Controllers/HomeController.cs
+++ notatsukinotanoshi/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Linq;
 using notatsukinotanoshi.Models.Utilities;
+using notatsukinotanoshi.Localizers;
 
 namespace notatsukinotanoshi.Controllers
 {
@@ -19,10 +20,12 @@ namespace notatsukinotanoshi.Controllers
     {
         private readonly string connectionString;
         private readonly IStringLocalizer<HomeController> _localizer;
+        private readonly IStringLocalizer<CompanyName> _companyName;
 
-        public HomeController(IStringLocalizer<HomeController> localizer, IConfiguration config)
+        public HomeController(IStringLocalizer<HomeController> localizer, IStringLocalizer<CompanyName> companyName, IConfiguration config)
         {
             _localizer = localizer;
+            _companyName = companyName;
             connectionString = config.GetValue<string>("ConnectionStrings:DefaultConnection"); //MySQL settings
         }
 
@@ -46,7 +49,7 @@ namespace notatsukinotanoshi.Controllers
                     {
                         model.Sponsors.Add(new SelectListItem
                         {
-                            Text = _localizer[reader.GetString(1)],
+                            Text = _companyName[reader.GetString(1)],
                             Value = reader.GetInt16(0).ToString()
                         });
                     }
@@ -124,90 +127,6 @@ namespace notatsukinotanoshi.Controllers
                 return Json("success");
             }
             return View(model);
-        }
-
-        /// <summary>
-        /// Generate the mail
-        /// </summary>
-        /// <param name="model"></param>
-        /// <returns></returns>
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Generate(EmailSubmitViewModel model)
-        {
-            var response = new ResponseAPI();
-
-            if (ModelState.IsValid)
-            {
-                //Get requested culture
-                var culture = Request.HttpContext.Features.Get<IRequestCultureFeature>().RequestCulture.Culture.Name;
-                string[] supportedCultures = { "en", "ja" };
-                if (!supportedCultures.Contains(culture))
-                {
-                    culture = "en";
-                }
-
-                var msg = "";
-                var companyName = "";
-                var companyMail = "";
-                using (var conn = new MySqlConnection(connectionString))
-                {
-                    try
-                    {
-                        conn.Open();
-
-                        //Get a random template
-                        var cmd = conn.CreateCommand();
-                        cmd.CommandText = "SELECT text_body FROM email_templates et WHERE et.locale = @locale AND approved = true ORDER BY RAND() LIMIT 1";
-                        cmd.Parameters.AddWithValue("@locale", culture);
-                        var reader = cmd.ExecuteReader();
-                        if (reader.Read())
-                        {
-                            msg = reader.GetString(0);
-                        }
-                        reader.Close();
-
-                        //Get the target company info
-                        cmd = conn.CreateCommand();
-                        cmd.CommandText = "SELECT name, email FROM company_info WHERE active = true LIMIT 1";
-                        cmd.Parameters.AddWithValue("@locale", culture);
-                        reader = cmd.ExecuteReader();
-                        if (reader.Read())
-                        {
-                            companyName = _localizer[reader.GetString(0)];
-                            companyMail = reader.GetString(1);
-                        }
-                        reader.Close();
-
-                    }
-                    catch (Exception)
-                    {
-                        throw;
-                    }
-                    finally
-                    {
-                        //Close the connection
-                        conn.Close();
-                    }
-                };
-
-                msg = msg.Replace("%company_name%", companyName)
-                    .Replace("%user_name%", model.FriendName)
-                    .Replace("%user_nationality%", model.FriendCountry);
-
-                var returnData = new Dictionary<string, string>();
-                returnData.Add("template", msg);
-                returnData.Add("email", companyMail);
-
-                response.Status = ResponseState.Success;
-                response.Message = "Get template successfully";
-                response.ReturnData = returnData;
-                return Json(response);
-            }
-
-            response.Status = ResponseState.Fail;
-            response.Message = "Missing details";
-            return Json(response);
         }
 
         /// <summary>
