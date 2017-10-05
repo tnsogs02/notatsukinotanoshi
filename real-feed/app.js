97946806383beaf42cdac0251d3a49d908e14184
Eric Lam <ericlamyauping@gmail.com>
2017-10-05 14:14:09 +1000

    show tweets on about

--- real-feed/app.js
+++ real-feed/app.js
@@ -1,12 +1,41 @@
-var app = require('http').createServer(handler),
-    io = require('socket.io')(app),
-    cfg = require('./config.json'),
-    fs = require('fs');
-
-app.listen(1337);
-
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var cfg = require('./config.json');
 var tw = require('node-tweet-stream')(cfg);
+var Twitter = require('twitter');
 
+/* app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+}); */
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
+/** Grap tweets on connect */
+var client = new Twitter(cfg);
+var params = {
+    q:"燃える紙飛行機",
+    result_type: 'mixed',
+    count:5,
+    include_entities: true
+};
+io.on('connection', function(socket){
+    console.log('a user connected');
+    client.get('search/tweets', params, function(error, tweets, response){
+        if(error) {
+            console.log(error)
+            throw error;
+        }
+        tweets['statuses'].forEach(function(element) {
+            console.log(element);
+        }, this);
+        socket.emit('tweet', tweets);
+    });
+});
+
+/** Track twitter */
 tw.track('燃える紙飛行機');
 tw.on('tweet', function (tweet) {
     console.log('tweet received', tweet);
@@ -15,17 +44,4 @@ tw.on('tweet', function (tweet) {
 
 tw.on('error', function (err) {
     console.log('Oh no');
-})
-
-function handler (req, res) {
-    fs.readFile(__dirname + '/index.html',
-    function (err, data) {
-      if (err) {
-        res.writeHead(500);
-        return res.end('Error loading index.html');
-      }
-  
-      res.writeHead(200);
-      res.end(data);
-    });
-}
+});
