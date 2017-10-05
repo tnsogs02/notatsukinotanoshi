5b2200a90b4fe94e8f819fec8514c61b2aaead4d
Eric Lam <ericlamyauping@gmail.com>
2017-10-05 21:45:03 +1000

    use twitter widgets

--- notatsukinotanoshi/wwwroot/js/about.js
+++ notatsukinotanoshi/wwwroot/js/about.js
@@ -1,19 +1,24 @@
 ﻿//The message
-const callout = '<div class="bs-callout bs-callout-warning tweets-content"><img style="float: left; margin:8px" src="[profile-pic]"><b>[tweet-name]</b> - <span data-time="[tweet-time]" class="tweet-time"></span><p>[replace-text]</p></div>';
+const callout = '<blockquote class="twitter-tweet" data-lang="[sys-locale]"><p lang="[tweet-locale]" dir="ltr">[replace-text]</p>&mdash; [tweet-name] ([screen-name]) <a class="tweet-time" href="https://twitter.com/[screen-name]/status/[tweet-id]" data-time="[tweet-time]"></a></blockquote>';
 
 var socket = io.connect('https://socket.savejaparipark.com');
 socket.on('tweet', function (data) {
     let user = data.user;
     let tweets = $("#tweets");
     tweets.prepend(
-        callout.replace('[replace-text]', data.text)
-            .replace('[tweet-name]', user.name)
-            .replace('[tweet-time]', data.created_at)
-            .replace('[profile-pic]', user.profile_image_url_https)
+        callout.replace(/\[replace-text\]/g, data.text)
+            .replace(/\[tweet-name\]/g, user.name)
+            .replace(/\[screen-name\]/g, user.screen_name)
+            .replace(/\[tweet-id\]/g, data.id_str)
+            .replace(/\[tweet-locale\]/g, data.lang)
+            .replace(/\[tweet-time\]/g, data.created_at)
+            .replace(/\[sys-locale\]/g, $('#culture-input').val())
     );
 
+    twttr.widgets.load(tweets);
+
     //Update time when a new tweet is published
-    tweets.find("span.tweet-time").each(function () {
+    tweets.find("a.tweet-time").each(function () {
         $(this).text(DateFormat.format.prettyDate($(this).data("time")));
     });
 
