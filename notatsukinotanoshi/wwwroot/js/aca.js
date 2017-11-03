1557c7d64e7bc0c48c3d09d18e591ced4fda48e5
Eric Lam <ericlamyauping@gmail.com>
2017-11-03 21:25:28 +1000

    open aca page after confirm

--- notatsukinotanoshi/wwwroot/js/aca.js
+++ notatsukinotanoshi/wwwroot/js/aca.js
@@ -4,6 +4,7 @@
     let sendModal = $("#modal");
     let modalBtnSubmit = $("#modalBtnSubmit");
     let content = $("#mailContentView");
+    let sendTo = "";
 
     btnSendConfirm.click(function (e) {
         e.preventDefault();
@@ -14,25 +15,13 @@
             loading.fadeIn();
             $.post("/Api/GenerateACA", function (data) {
                 sendModal.find(".modal-header h1").text("寄信給 " + selectedTarget.val() + " - 確認信件內容");
-              
                 content.html(data['returnData']['template']);
+                sendTo = selectedTarget.data("link"); 
 
                 // 設定信件資料
                 sendModal.modal();
                 loading.fadeOut();
             }, 'json');
-            /* (function () {
-                // AJAX取得自動生成的信件資料
-                // 程式目前為等候兩秒後直接出現視窗
-                setTimeout(function () {
-                    sendModal.find(".modal-header h1").text("寄信給 " + selectedTarget.val() + " - 確認信件內容");
-
-                    // 設定信件資料
-
-                    sendModal.modal();
-                    loading.fadeOut();
-                }, 2000);
-            }, 1); */
         } else {
             alert("您還沒有選取您要寄信的對象，幸運獸無法處理喔！");
         }
@@ -41,9 +30,11 @@
     $("#modalBtnSubmit").click(function (e) {
         e.preventDefault();
 
-        // AJAX送出訊息的處理
+        // Copy to clipboard
         copyToClipboard(content.get(0));
-        console.log(content.get(0));
+
+        //Open ACA page
+        window.open(sendTo);
         sendModal.modal("hide");
     });
 });
